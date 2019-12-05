cc65b8fea2383cc788e09dc3db1126a0864f3463
Malbolge <victor.oliveiragomes@outlook.com>
2019-12-04 20:39:40 -0400

    fixed an error on getting mp4 metadata
    Fixed an error on getting mp4 metadata. Maybe sometimes not all metadata will be present and a more generic solution was required.

--- scraping_anie.py
+++ scraping_anie.py
@@ -1,13 +1,12 @@
 import bs4 as bs
-import urllib.request
-import re
-import json
-import os
-import subprocess
+import urllib.request, re, json, os, subprocess
 
 url = 'https://www.crunchyroll.com/pt-br/my-hero-academia'
-diretorio_anime = 'C:\\Users\\lucas\\Desktop\\GUARDA_TRECO(L)\\rouba_info\\asobi_teste\\'
-exe = 'C:\\Users\\lucas\\Desktop\\GUARDA_TRECO(L)\\rouba_info\\exiftool\\exiftool.exe'
+# diretorio_anime = 'C:\\Users\\lucas\\Desktop\\GUARDA_TRECO(L)\\rouba_info\\asobi_teste\\'
+# exe = 'C:\\Users\\lucas\\Desktop\\GUARDA_TRECO(L)\\rouba_info\\exiftool\\exiftool.exe'
+diretorio_anime = 'F:\\Documentos\\AnieZillaProject\\videoTests\\'
+exe = 'F:\\Documentos\\AnieZillaProject\\exiftool\\exiftool.exe'
+
 AbaEscolhida = 0 # crunchyroll fudendo o batalhão, valor: 0 as abas estão corretas no site
 TemporadaReal = 1
 
@@ -25,10 +24,9 @@ for string in listaEpisodios:
     if x:
         NumeroEpisodios.add(int(''.join(x)))
 
-NumeroEpisodios
 print(NumeroEpisodios) # Lista de Episodios no diretorio
 
-temporadas = soup.find_all('li', class_='season') # lista de temporadas
+temporadas = soup.find_all('li', class_ = 'season') # lista de temporadas
 temporadas = temporadas[::-1] # deixa temporadas em ordem crescente
 
 if AbaEscolhida == 0:
@@ -48,20 +46,24 @@ for idy, temporada in enumerate(temporadas): #vare temporadas
 
         nomes = temporada.find_all('img') #pega nomes dos episodios
         nomes = nomes[::-1] #deixa episodios em ordem crescente
-        for episodio, nome in enumerate(nomes):
-            if episodio + 1 in NumeroEpisodios: # Se o episodio atual é um episodio que foi baixado, pega as infos
-                print(TemporadaReal,'-', episodio + 1, nome.get('alt'))
-                input_file = diretorio_anime + 'episodio-' + str(episodio + 1) +'.mp4'
-                process = subprocess.Popen([exe, input_file], stdout = subprocess.PIPE, stderr = subprocess.STDOUT, universal_newlines = True)
-                for idx, output in enumerate(process.stdout):
-                    if idx == 18 or idx == 36:
-                        meta = str(output)
-                        if idx == 18: #metadado referente à duração do video
-                            duracao = meta[36:-1]
-                        else: #metadado referente à qualidade do video
-                            qualidade = meta[34:-1]
+        for episodio, nome in enumerate(nomes, 1):
 
-                dados_ep = {'temporada' : TemporadaReal, 'episodio' : episodio + 1, 'nome' : str(nome.get('alt')), 'duracao' : duracao,  'thumb' : 'thumb-'+ str(episodio+1) + '.png', 'qualidade' : qualidade + 'p' }
+            if episodio in NumeroEpisodios: # Se o episodio atual é um episodio que foi baixado, pega as 
+                
+                print(TemporadaReal,'-', episodio, nome.get('alt'))
+
+                input_file = diretorio_anime + 'episodio-' + str(episodio) +'.mp4'
+                process = subprocess.Popen([exe, input_file], stdout = subprocess.PIPE, stderr = subprocess.STDOUT, universal_newlines = True)
+              
+                info = {}
+                for output in process.stdout:
+                    
+                    if re.search(('(Media Duration|Source Image Height)'), output):
+                        string = re.split(':', output, maxsplit = 1)
+                        string = string[0].strip(), string[1].strip()
+                        info[string[0]] = string[1]
+
+                dados_ep = {'temporada' : TemporadaReal, 'episodio' : episodio, 'nome' : str(nome.get('alt')), 'duracao' : info['Media Duration'][2:],  'thumb' : 'thumb-'+ str(episodio) + '.png', 'qualidade' : info['Source Image Height'] + 'p' }
                 dados_like_json = json.dumps(dados_ep, ensure_ascii=False).encode('utf8')
                 configCFG.write(dados_like_json.decode())
                 configCFG.write('\n')
