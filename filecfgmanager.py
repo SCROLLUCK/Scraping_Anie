9b2b223fe61dab5a5f0eee3dd6c500206535d17e
Malbolge <victor.oliveiragomes@outlook.com>
2019-12-20 19:25:38 -0400

    Classe que lida com um objeto cfg

---
+++ filecfgmanager.py
@@ -0,0 +1,52 @@
+import os, subprocess, re, json
+
+class CFGfile(object):
+
+    def __init__(self, path, episodeList):
+        
+        self.path = path
+        self.episodeNumbers = set({})
+        self.episodeList = episodeList
+        self.episodeInfoList = []
+        self.exe = os.getcwd() + os.sep + 'exiftool/' + 'exiftool.exe'
+    
+    def getEpisodesNnumbers(self):
+
+        for number in self.episodeList:
+            x = re.findall('\\d+', number[:-1])
+            if x:
+                self.episodeNumbers.add(int(''.join(x)))
+
+    def getEpisodeInfo(self, names, season):
+
+        for number, name in enumerate(names, 1):
+            
+            if number in self.episodeNumbers:
+
+                input_file = self.path + 'episodio-' + str(number) + '.mp4'
+                process = subprocess.Popen([self.exe, input_file], stdout = subprocess.PIPE, stderr = subprocess.STDOUT, universal_newlines = True)
+
+                info = {}
+                for ouput in process.stdout:
+                    if re.search(('(Media Duration|Source Image Height)'), ouput):
+                        string = re.split(':', ouput, maxsplit = 1)
+                        info[string[0].strip()] = string[1].strip()
+
+                episodeData = {'temporada' : season + 1, 'episodio' : number, 'nome' : name, 'duracao' : info['Media Duration'][2:], 'thumb' : 'thumb-' + str(number) + '.png', 'qualidade' : info['Source Image Height'] + 'p' }
+                self.episodeInfoList.append(episodeData)
+
+    def getEpisodeInfoList(self):
+
+        return self.episodeInfoList
+
+    def setCfgFile(self, path, jsonList):
+        
+        with open(path + 'config.cfg', 'w', encoding = 'utf-8') as cfg:
+
+            for info in jsonList:
+                cfg.write(json.dumps(info, ensure_ascii = False))
+                cfg.write('\n')
+    
+    def epsodeNameVerify(self):
+
+        return all([re.search('\\bepisodio-\\d+[.]mp4\\b', j) for j in self.episodeList])
